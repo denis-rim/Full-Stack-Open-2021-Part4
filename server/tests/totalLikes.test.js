1953d740e52e0664d47d0cac9b918850d27c6229
denis-rim <denis.aurorim@gmail.com>
2021-04-19 20:21:07 +0300

    4.4: helper functions and unit tests, step2

---
+++ server/tests/totalLikes.test.js
@@ -0,0 +1,51 @@
+const listHelper = require("../utils/list_helper");
+
+describe("total likes", () => {
+  const listWithEmptyBlogs = [];
+  const listWithOneBlog = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url:
+        "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+  const listWithManyBlogs = [
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url:
+        "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+    {
+      _id: "5a422aa71b54a676234d17f8",
+      title: "Go To Statement Considered Harmful",
+      author: "Edsger W. Dijkstra",
+      url:
+        "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+      likes: 5,
+      __v: 0,
+    },
+  ];
+
+  test("of empty list is zero", () => {
+    const result = listHelper.totalLikes(listWithEmptyBlogs);
+    expect(result).toBe(0);
+  });
+
+  test("when list has only one blog, equals the likes of that", () => {
+    const result = listHelper.totalLikes(listWithOneBlog);
+    expect(result).toBe(5);
+  });
+
+  test("of bigger list is calculated right", () => {
+    const result = listHelper.totalLikes(listWithManyBlogs);
+    expect(result).toBe(10);
+  });
+});
--- server/utils/list_helper.js
+++ server/utils/list_helper.js
@@ -2,6 +2,13 @@ const dummy = (blogs) => {
   return 1;
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((acc, cur) => {
+    return acc + cur.likes;
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };

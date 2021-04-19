78242c308a8e95f5f2f691361aa5d4efc8b96f3e
denis-rim <denis.aurorim@gmail.com>
2021-04-19 12:08:24 +0300

    4.1 Blog list, step1

--- server/index.js
+++ server/index.js
@@ -1,43 +1,53 @@
-const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require("http");
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
 
 const blogSchema = new mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
 
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = mongoose.model("Blog", blogSchema);
 
-const mongoUrl = 'mongodb://localhost/bloglist'
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+const mongoUrl =
+  "mongodb+srv://username:password@blog-list.9mpwg.mongodb.net/blog-list?retryWrites=true&w=majority";
 
-app.use(cors())
-app.use(express.json())
+mongoose
+  .connect(mongoUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
+app.use(cors());
+app.use(express.json());
 
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
 
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
 
-const PORT = 3003
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = 3003;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
